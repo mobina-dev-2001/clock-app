e636bb4ddd66ab4f2fdf2cba8987c335812bca64
insomnish dev <mobina.smt@gmail.com>
2025-05-19 13:26:17 +0330

    Minor Fixes

--- api/ipinfo.js
+++ api/ipinfo.js
@@ -1,17 +1,16 @@
 export default async function handler(req, res) {
-    try {
-      const response = await fetch("https://api.ipbase.com/v2/info", {
-        headers: {
-          "apikey": import.meta.env.IPBASE_API_KEY,
-        },
-      });
-  
-      if (!response.ok) throw new Error("Failed to fetch IP info");
-      const data = await response.json();
-  
-      res.status(200).json(data);
-    } catch {
-      res.status(500).json({ error: "Failed to fetch IP info" });
-    }
+  try {
+    const response = await fetch("https://api.ipbase.com/v2/info", {
+      headers: {
+        apikey: process.env.IPBASE_API_KEY,
+      },
+    });
+
+    if (!response.ok) throw new Error("Failed to fetch IP info");
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch {
+    res.status(500).json({ error: "Failed to fetch IP info" });
   }
-  
+}
--- api/quotes.js
+++ api/quotes.js
@@ -1,16 +1,16 @@
 export default async function handler(req, res) {
-    try {
-      const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
-        headers: {
-          "X-Api-Key": import.meta.env.QUOTES_API_KEY,
-        },
-      });
-  
-      if (!response.ok) throw new Error("Failed to fetch quote");
-      const data = await response.json();
-  
-      res.status(200).json(data);
-    } catch {
-      res.status(500).json({ error: "Failed to fetch quote" });
-    }
+  try {
+    const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
+      headers: {
+        "X-Api-Key": process.env.QUOTES_API_KEY,
+      },
+    });
+
+    if (!response.ok) throw new Error("Failed to fetch quote");
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch {
+    res.status(500).json({ error: "Failed to fetch quote" });
   }
+}

32b4f48baaa479eafaaf90c43583f9028f7ffce3
insomnish dev <mobina.smt@gmail.com>
2025-05-19 13:05:37 +0330

    Minor fixes

--- api/ipinfo.js
+++ api/ipinfo.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     try {
       const response = await fetch("https://api.ipbase.com/v2/info", {
         headers: {
-          "apikey": process.env.IPBASE_API_KEY,
+          "apikey": import.meta.env.IPBASE_API_KEY,
         },
       });
   
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       const data = await response.json();
   
       res.status(200).json(data);
-    } catch (err) {
+    } catch {
       res.status(500).json({ error: "Failed to fetch IP info" });
     }
   }
--- api/quotes.js
+++ api/quotes.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     try {
       const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
         headers: {
-          "X-Api-Key": process.env.QUOTES_API_KEY,
+          "X-Api-Key": import.meta.env.QUOTES_API_KEY,
         },
       });
   
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       const data = await response.json();
   
       res.status(200).json(data);
-    } catch (err) {
+    } catch {
       res.status(500).json({ error: "Failed to fetch quote" });
     }
   }
